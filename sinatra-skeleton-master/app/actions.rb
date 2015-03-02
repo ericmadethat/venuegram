070f435eb81b68fd032b91614a7289af1948269d
nuen <hello@ericdesigns.ca>
2015-03-02 04:06:40 +0000

    Added the ability to click on the link and see the instagram photo

--- sinatra-skeleton-master/app/actions.rb
+++ sinatra-skeleton-master/app/actions.rb
@@ -23,8 +23,7 @@ get "/photos/index" do
   client = Instagram.client(:access_token => session[:access_token])
   photos_array = []
   for media_item in client.media_search(lat,lon, count: 12)
-    photos_array << "<img src='#{media_item.images.low_resolution.url}'>"
+    photos_array << ["<img src='#{media_item.images.low_resolution.url}'>", "#{media_item.link}"]
   end
   json photos_array
 end
-
